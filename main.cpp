c709597228a68aaa4bd15b1a2f223e5ee7f81d6e
Mahdi Nejad Sahebi <m.nejadsahebi@live.co.uk>
2023-10-27 23:28:52 +0330

    Extraction of movements was done.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <cstring>
 #include <logic/logic.hpp>
 
 int main(int _n, char* _args[])
 {
-  std::string filePath(_args[0]);
+//  std::string filePath(_args[0]);
+  std::string filePath = "/home/mahdi/in.txt";
+  std::cout << filePath << std::endl;
   Logic logic(filePath);
   logic.Solve();
 
