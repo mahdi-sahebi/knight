2757b63fe3952dc020badbbc51fa0a9536f34c65
Mahdi Nejad Sahebi <m.nejadsahebi@live.co.uk>
2023-10-29 03:43:51 +0330

    Checkpoint.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int _n, char* _args[])
   // TODO(MN): Output example 1 is wrong in PDF. b6-e6 is imposible.
   // TODO(MN): Output example 3 is wrong in PDF. There is no black player to hit.
   // TODO(MN): Output example 4 is wrong in PDF. Number of players is 4, not 5
+  // TODO(MN): Output example 5 is wrong in PDF. Number of players is 4, not 5
   std::string filePath = "/home/mahdi/in.txt";
   Logic logic(filePath);
   logic.Solve();
