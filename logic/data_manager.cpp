1a49bdf29e9e0702b874002d1dff9db701b088ac
Mahdi Nejad Sahebi <m.nejadsahebi@live.co.uk>
2023-10-30 01:06:53 +0330

    Extractor APIs were defined.

--- logic/data_manager.cpp
+++ logic/data_manager.cpp
@@ -17,11 +17,45 @@ static uint8_t extractPlayersNumber(string_view::iterator& _itrBegin,
                                     const string_view::iterator& _itrEnd,
                                     const function<bool(char)>& _comp)
 {
-  string_view::iterator itrEnd = find_if(_itrBegin, _itrEnd, _comp);
-  string temp = string(_itrBegin, itrEnd);
+  const string_view::iterator itrEnd = find_if(_itrBegin, _itrEnd, _comp);
+  const string temp = string(_itrBegin, itrEnd);
   _itrBegin = itrEnd + 1;
   const uint8_t playersNumber = atoi(temp.c_str());
   return playersNumber;
+}
+
+static Player::Color extractColor(string_view::iterator& _itrBegin,
+                                  const string_view::iterator& _itrEnd,
+                                  const function<bool(char)>& _comp)
+{
+  const string_view::iterator itrEnd = find_if(_itrBegin, _itrEnd, _comp);
+  const string temp = string(_itrBegin, itrEnd);
+  _itrBegin = itrEnd + 1;
+  const Player::Color color = static_cast<Player::Color>(atoi(temp.c_str()));
+  return color;
+}
+
+static Player::Type extractType(string_view::iterator& _itrBegin,
+                                const string_view::iterator& _itrEnd,
+                                const function<bool(char)>& _comp)
+{
+  const string_view::iterator itrEnd = find_if(_itrBegin, _itrEnd, _comp);
+  const string temp = string(_itrBegin, itrEnd);
+  _itrBegin = itrEnd + 1;
+  const Player::Type type = static_cast<Player::Type>(atoi(temp.c_str()));
+  return type;
+}
+
+static Location extractLocation(string_view::iterator& _itrBegin,
+                                             const string_view::iterator& _itrEnd,
+                                             const function<bool(char)>& _comp)
+{
+  const string_view::iterator itrEnd = find_if(_itrBegin, _itrEnd, _comp);
+  const string temp = string(_itrBegin, itrEnd);
+  _itrBegin = itrEnd + 1;
+  const Column column = static_cast<Column>(temp.c_str()[0] - 'a');
+  const Row    row    = static_cast<Row>   (atoi(temp.substr(1, 1).c_str()) - 1);
+  return Location(column, row);
 }
 
 tuple<uint8_t, vector<PlayerDescriptor>> Import(const string _filePath)
@@ -37,9 +71,8 @@ tuple<uint8_t, vector<PlayerDescriptor>> Import(const string _filePath)
     const string_view buffer = readBuffer;
 
     string temp = "";
-    string_view::iterator itrBegin = begin(buffer);
-    const string_view::iterator endBufer = end(buffer);
-    string_view::iterator itrEnd   = itrBegin;
+    string_view::iterator       itrBegin = begin(buffer);
+    const string_view::iterator itrEnd   = end(buffer);
 
     const auto isSpace = [](char _char)
     {
@@ -51,34 +84,21 @@ tuple<uint8_t, vector<PlayerDescriptor>> Import(const string _filePath)
       return ('\n' == _char);
     };
 
-    const uint8_t playersNumber = extractPlayersNumber(itrBegin, endBufer, isLF);
+    const uint8_t playersNumber = extractPlayersNumber(itrBegin, itrEnd, isLF);
 
     /* Extraction of Players */
     vector<PlayerDescriptor> players;
     for (uint8_t playerIdx = 0; playerIdx < playersNumber; playerIdx++)
     {
-      /* Color */
-      itrEnd = find_if(itrBegin, end(buffer), isSpace);
-      temp = string(itrBegin, itrEnd);
-      itrBegin = itrEnd + 1;
-      const Player::Color color = static_cast<Player::Color>(atoi(temp.c_str()));
+      const Player::Color color = extractColor(itrBegin, itrEnd, isSpace);
 
-      /* Type */
-      itrEnd = find_if(itrBegin, end(buffer), isSpace);
-      temp = string(itrBegin, itrEnd);
-      itrBegin = itrEnd + 1;
-      Player::Type type = static_cast<Player::Type>(atoi(temp.c_str()));
+      Player::Type type = extractType(itrBegin, itrEnd, isSpace);
       if (Player::Color::White == color)
         type = Player::Type::KNIGHT;
 
-      /* Location */
-      itrEnd = find_if(itrBegin, end(buffer), isLF);
-      temp = string(itrBegin, itrEnd);
-      itrBegin = itrEnd + 1;
-      const Column column = static_cast<Column>(temp.c_str()[0] - 'a');
-      const Row    row    = static_cast<Row>   (atoi(temp.substr(1, 1).c_str()) - 1);
+      const Location location = extractLocation(itrBegin, itrEnd, isLF);
 
-      players.push_back(PlayerDescriptor(color, type, Location(column, row)));
+      players.push_back(PlayerDescriptor(color, type, location));
     }
 
     /* Extraction of Movement */
